02e3acff483ba89e2b29cfd0b4b709b298230420
Ahmar khan <ahmarbilal223344@gmail.com>
2023-01-28 10:30:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg"
+import "./App.css"
+import Nav from "./Nav"
+import Meme from "./Meme"
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
